7f6f805b1e64dda61f0d1a587cabd763ec9f2646
H-arshit <harshitsinghal33@gmail.com>
2018-08-09 09:11:56 +0530

    Completed 1) Sending request for key . 2) receiving the send key and displaying contest ended if 2 keys are recieved

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const web3Functions = require('./web3Server.js');
 const telegram = require('./telegramServer.js');
 
+let key_shard_map = new Object() ;
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
@@ -13,6 +15,53 @@ app.use('/static', express.static('public_static'))
 
 app.get('/msg', function (req, res) {
    // TODO for harshit... here we will redirect the webhook and all messages are to be handeled from here
+   if(!req.body.message)
+     {
+         res.sendStatus(200);
+     }
+   else
+   {
+
+
+        let id = req.body.message.from.id;
+        let text = req.body.message.text;
+
+        let key_shard = text.split(" ") ;
+
+        let key = key_shard[0] ;
+        let shard = key_shard[2] ;
+
+
+        if(key_shard_map[key] == null)
+        {
+          key_shard_map[key] = [shard] ;
+        }
+        else
+        {
+          if(key_shard_map[key].length == 2)
+          {
+            telegram.FirstTwoKeyReceived(id , function () {} )
+          }
+          else
+          {
+            let shard_arr = key_shard_map[key] ;
+            shard_arr.push(shard) ;
+            key_shard_map[key] = shard_arr  ;
+
+
+            // TODO 2 keys received .. send to web3 ... frontend
+          }
+        }
+
+
+
+
+   }
+
+
+
+
+
 });
 app.get('/',function(req,res){
   res.sendFile(__dirname+ '/public_static/views/index.html');
--- telegramServer.js
+++ telegramServer.js
@@ -1,7 +1,51 @@
-
+const axios = require('axios');
 
 module.exports = {
-  sendKeyToRetrieve : function (key , chatIds, callback) {
+
+  token_id : '516574402:AAEFJIWFYnz19-ZSg3-Lg8IdH2vt6VY4jmI',
+
+
+  sendKeyToRetrieve : function sendKeyToRetrieve(key , chatIds, callback) {
       // TODO write the code here to send all the chatIds the key so that they can send their respective shards
+
+      let url = 'https://api.telegram.org/bot' + this.token_id + '/sendMessage';
+      let request_text = "Please send the shard corresponding to the key : " + key ". Please reply in the following format 'key : shard''";
+
+        for(i = 0 ; i < chatIds.length ; i++)
+        {
+              axios.get(url, {
+                params : {
+                    chat_id : chatIds[i],
+                    text : request_text,
+                }
+            }).then(function (response) {
+                if(callback) callback(true);
+            }).catch(function (error) {
+                throw error
+            });
+      }
+  },
+
+  FirstTwoKeyReceived : function FirstTwoKeyReceived( chatId, callback) {
+    // sending contentest ended after receiving first two message for a key
+
+      let url = 'https://api.telegram.org/bot' + this.token_id + '/sendMessage';
+      let text = "Sorry the content ended. Better luck next time" ;
+
+              axios.get(url, {
+                params : {
+                    chat_id : chatId,
+                    text : text,
+                }
+            }).then(function (response) {
+                if(callback) callback(true);
+            }).catch(function (error) {
+                throw error
+            });
+
+
   }
+
+
+
 };
